647f69b3b245e698a18352dc801689eda807cc3e
Paul Kinlan <paul.kinlan@gmail.com>
2018-08-13 13:46:05 +0100

    First commit

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+app.post('/', (req, res) => {
+  const { webhook_url } = req.query;
+  const { body } = req;
+
+  console.log(body);
+
+  if (body.items === undefined || body.items.length === 0) {
+    res.send('');
+    return;
+  }
+
+  fetch(webhook_url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({
+      "text": `*${body.title}* published <${body.items[0].permalinkUrl}|${body.items[0].title}>. Please consider <https://twitter.com/intent/tweet?url=${encodeURIComponent(body.items[0].permalinkUrl)}&text=${encodeURIComponent(body.items[0].title)}|Sharing it>.`
+    })  
+  }).then(() => {
+    return res.send('ok');
+  }).catch(() => {
+    return res.send('error')
+  });
+})
+// Expose Express API as a single Cloud Function:
+exports.publish = functions.https.onRequest(app);
